897c1fd320233405319e5dc69a8c216289ae4d9e
= <=>
2022-02-13 00:21:26 +0700

    adding validation to articles

--- db/migrate/20220212170636_create_articles.rb
+++ db/migrate/20220212170636_create_articles.rb
@@ -1,9 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :content
-      t.string :slug
+      t.string :title, null: false 
+      t.text :content, null: false 
+      t.string :slug, null: false, unique: true
 
       t.timestamps
     end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,12 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'should be positive number' do 
-    expect(1).to be_positive
+  describe "#test" do
+    it 'should be positive number' do 
+      expect(1).to be_positive
+    end
+
+    it 'should have valid factory' do 
+      article = create :article
+      expect(article.title).to eq('Sample Article')
+    end
   end
 
-  it 'should have valid factory' do 
-    article = create :article
-    expect(article.title).to eq('Sample Article')
+  describe "#validations" do 
+    it 'should not have blank title' do 
+      article = build(:article, title: '')
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it 'should not have blank content' do 
+      article = build(:article, content: '')
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it 'should not have blank slug' do 
+      article = build(:article, slug: '')
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+
+    it 'should have unique slug' do 
+      article = create(:article)
+      other_article = build(:article, slug: article.slug)
+      expect(other_article).not_to be_valid
+      expect(other_article.errors[:slug]).to include("has already been taken")
+    end
   end
 end
