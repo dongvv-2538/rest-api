5994d5b8ca996fd4c9f56399412880ccc68ca01a
= <=>
2022-02-13 00:51:12 +0700

    listing all article at index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
   def index
+    articles = Article.all 
+    render json: serializer.new(articles), status: :ok
   end
 
   def show
+  end
+
+  private 
+
+  def serializer
+    ArticleSerializer
   end
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
-  describe "GET /index" do
+  describe "#index" do
     it "returns http success" do
       get "/articles/index"
       expect(response).to have_http_status(:success)
+    end
+
+    it "should return list of article in proper json body" do 
+      article = create :article
+
+      get '/articles'
+
+      expect(json_data.length).to eq(1)
+      expected = json_data.first 
+
+      aggregate_failures do 
+        expect(expected[:id]).to eq(article.id.to_s)
+        expect(expected[:type]).to eq('article')
+        expect(expected[:attributes]).to eq(
+          title: article.title,
+          content: article.content,
+          slug: article.slug
+        )
+      end
     end
   end
 
